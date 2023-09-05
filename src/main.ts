f25cf24797ea1fdc4c433f548374f1bc390d234b
Esgileot <jaroslavlegends@gmail.com>
2023-09-05 14:57:08 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { NestFactory } from '@nestjs/core'
 import { exceptionFactory } from './common/exceptionFactory'
 import { AppModule } from './modules/main/app.module'
 import { setupSwagger } from './swagger'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: `${process.env.DOTENV_CONFIG_PATH}` })
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    credentials: true,
+  })
 
   app.useGlobalPipes(
     new ValidationPipe({
